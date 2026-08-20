259109448b729aaac80ce3166c4a36ffe87efd29
Nguyễn Bảo <baolongkiem8@gmail.com>
2026-08-20 13:31:57 +0700

    Update Seminar.cs
    fix

--- ARSPlatform.MODEL/Entities/Seminar.cs
+++ ARSPlatform.MODEL/Entities/Seminar.cs
@@ -15,8 +15,6 @@ public partial class Seminar
 
     public string Content { get; set; } = null!;
 
-    public bool IsOnline { get; set; } = false;
-
     public string? OnlineLink { get; set; }
 
     public int? MaxParticipants { get; set; }
